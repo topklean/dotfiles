30e8253d3294f304663a47e115b45e2ae8af2d9f
topklean@gmail.com <topklean@gmail.com>
2025-05-10 21:48:27 +0200

    i3/ script to exec a terminal and move it and give it the focus

---
+++ .config/i3/exec_terminal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+delay=${1}
+direction=${2}
+
+[[ $delay =~ [0-9]+(\.?[0-9]+)? ]] ||  { delay=0.5; }
+
+[[ $direction =~ left|right|up|down ]] || direction=
+
+sleep $delay
+i3-msg 'exec --no-startup-id kitty --detach'
+
+[[ $direction ]] && { 
+	sleep $delay
+	i3-msg "move $direction"
+}
